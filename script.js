28ea115c97942c88494a214a6c1b69860ae93d98
Breyanna Vass <breyannavass@gmail.com>
2020-01-09 12:06:52 -0500

    creating shapes in js with fillstyle and rect

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// fillRect()
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 150, 100);
+ctx.fillStyle = "blue";
+ctx.fillRect(200, 20, 150, 100);
