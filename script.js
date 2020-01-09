d54c9ba0442b85984b32a4135792cb9d8798c6d6
Breyanna Vass <breyannavass@gmail.com>
2020-01-09 12:13:41 -0500

    adding various rectangles and texts to the window

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ ctx.fillStyle = "red";
 ctx.fillRect(20, 20, 150, 100);
 ctx.fillStyle = "blue";
 ctx.fillRect(200, 20, 150, 100);
+
+// strokeRect()
+ctx.lineWidth = 5;
+ctx.strokeStyle = "green";
+ctx.strokeRect(100, 200, 150, 100);
+
+// clearRect()
+ctx.clear(25, 25, 140, 90);
+
+// fillText()
+ctx.font = "30px Arial";
+ctx.fillStyle = "purple";
+ctx.fillText("Hello World", 400, 50);
+
+// strokeText()
+ctx.lineWidth = 1;
+ctx.strokeStyle = "orange";
+ctx.strokeText("Hello World", 400, 100);
