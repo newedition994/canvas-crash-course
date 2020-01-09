f302bfac08a1952fc363ff7a76f085b8090e1c89
Breyanna Vass <breyannavass@gmail.com>
2020-01-09 12:35:39 -0500

    making a circle the bounces around the screen

--- script.js
+++ script.js
@@ -27,62 +27,101 @@ const ctx = canvas.getContext("2d");
 
 // Paths
 
-// Triangle
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(100, 200);
-ctx.lineTo(50, 50);
-ctx.closePath();
-ctx.fillStyle = "coral";
-ctx.fill();
+// // Triangle
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(100, 200);
+// ctx.lineTo(50, 50);
+// ctx.closePath();
+// ctx.fillStyle = "coral";
+// ctx.fill();
 
-ctx.beginPath();
-ctx.moveTo(200, 50);
-ctx.lineTo(150, 200);
-ctx.lineTo(250, 200);
-ctx.closePath();
-ctx.stroke();
+// ctx.beginPath();
+// ctx.moveTo(200, 50);
+// ctx.lineTo(150, 200);
+// ctx.lineTo(250, 200);
+// ctx.closePath();
+// ctx.stroke();
 
-// Rectangle
-ctx.beginPath();
-ctx.rect(300, 50, 150, 100);
-ctx.fillStyle = "teal";
-ctx.fill();
+// // Rectangle
+// ctx.beginPath();
+// ctx.rect(300, 50, 150, 100);
+// ctx.fillStyle = "teal";
+// ctx.fill();
 
-// Arc - circles
-ctx.beginPath();
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+// // Arc - circles
+// ctx.beginPath();
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
 
-// Draw head
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+// // Draw head
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
 
-// Move to mouth
-ctx.moveTo(centerX + 100, centerY);
+// // Move to mouth
+// ctx.moveTo(centerX + 100, centerY);
 
-// Draw mouth
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+// // Draw mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
 
-// Move left eye
-ctx.moveTo(centerX - 60, centerY - 80);
+// // Move left eye
+// ctx.moveTo(centerX - 60, centerY - 80);
 
-// Draw left eye
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+// // Draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
 
-// Move to right eye
-ctx.moveTo(centerX + 100, centerY - 80);
+// // Move to right eye
+// ctx.moveTo(centerX + 100, centerY - 80);
 
-// Draw right eye
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
+// // Draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
 
-// Quadratic curve
-ctx.moveTo(75, 25);
-ctx.quadraticCurveTo(25, 25, 25, 62.5);
-ctx.quadraticCurveTo(25, 100, 50, 100);
-ctx.quadraticCurveTo(50, 120, 30, 125);
-ctx.quadraticCurveTo(60, 120, 65, 100);
-ctx.quadraticCurveTo(125, 100, 125, 62.5);
-ctx.quadraticCurveTo(125, 25, 75, 25);
+// // Quadratic curve
+// ctx.moveTo(75, 25);
+// ctx.quadraticCurveTo(25, 25, 25, 62.5);
+// ctx.quadraticCurveTo(25, 100, 50, 100);
+// ctx.quadraticCurveTo(50, 120, 30, 125);
+// ctx.quadraticCurveTo(60, 120, 65, 100);
+// ctx.quadraticCurveTo(125, 100, 125, 62.5);
+// ctx.quadraticCurveTo(125, 25, 75, 25);
 
-ctx.stroke();
+// ctx.stroke();
+
+// Animation A
+
+const circle = {
+  x: 200,
+  y: 200,
+  size: 30,
+  dx: 5,
+  dy: 4
+};
+
+function drawCircle() {
+  ctx.beginPath();
+  ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+  ctx.fillStyle = "purple";
+  ctx.fill();
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawCircle();
+  // change position
+  circle.x += circle.dx;
+  circle.y += circle.dy;
+  // Detect side walls
+  if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+    circle.dx *= -1;
+  }
+
+  // Detect top and bottom walls
+  if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+    circle.dy *= -1;
+  }
+
+  requestAnimationFrame(update);
+}
+
+update();
