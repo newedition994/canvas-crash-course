2225e0324aeb63ceb35f1526d0c5dd8d312d3602
Breyanna Vass <breyannavass@gmail.com>
2020-01-09 12:43:45 -0500

    allow for the ability to move around the screen

--- script.js
+++ script.js
@@ -173,3 +173,59 @@ function detectWalls() {
     player.y = canvas.height - player.h;
   }
 }
+
+function update() {
+  clear();
+
+  drawPlayer();
+
+  newPos();
+
+  requestAnimationFrame(update);
+}
+
+function moveUp() {
+  player.dy = -player.speed;
+}
+
+function moveDown() {
+  player.dy = player.speed;
+}
+
+function moveRight() {
+  player.dx = player.speed;
+}
+
+function moveLeft() {
+  player.dx = -player.speed;
+}
+
+function keyDown(e) {
+  if (e.key === "ArrowRight" || e.key === "Right") {
+    moveRight();
+  } else if (e.key === "ArrowLeft" || e.key === "Left") {
+    moveLeft();
+  } else if (e.key === "ArrowUp" || e.key === "Up") {
+    moveUp();
+  } else if (e.key === "ArrowDown" || e.key === "Down") {
+    moveDown();
+  }
+}
+
+function keyUp(e) {
+  if (
+    e.key == "Right" ||
+    e.key == "ArrowRight" ||
+    e.key == "Left" ||
+    e.key == "ArrowLeft" ||
+    e.key == "Up" ||
+    e.key == "ArrowUp" ||
+    e.key == "Down" ||
+    e.key == "ArrowDown"
+  ) {
+    player.dx = 0;
+    player.dy = 0;
+  }
+}
+
+update();
