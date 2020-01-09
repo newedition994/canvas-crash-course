f7a3953ebcd79215c2b6e6c3e195fd9e1f79f533
Breyanna Vass <breyannavass@gmail.com>
2020-01-09 12:26:56 -0500

    adding in various shapes to the window"
    "adding shapes"

--- script.js
+++ script.js
@@ -57,3 +57,32 @@ const centerY = canvas.height / 2;
 
 // Draw head
 ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+
+// Move to mouth
+ctx.moveTo(centerX + 100, centerY);
+
+// Draw mouth
+ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+
+// Move left eye
+ctx.moveTo(centerX - 60, centerY - 80);
+
+// Draw left eye
+ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+
+// Move to right eye
+ctx.moveTo(centerX + 100, centerY - 80);
+
+// Draw right eye
+ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
+
+// Quadratic curve
+ctx.moveTo(75, 25);
+ctx.quadraticCurveTo(25, 25, 25, 62.5);
+ctx.quadraticCurveTo(25, 100, 50, 100);
+ctx.quadraticCurveTo(50, 120, 30, 125);
+ctx.quadraticCurveTo(60, 120, 65, 100);
+ctx.quadraticCurveTo(125, 100, 125, 62.5);
+ctx.quadraticCurveTo(125, 25, 75, 25);
+
+ctx.stroke();
