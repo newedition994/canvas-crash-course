a3b24e0732d45f529a47ea903cf2a438c87b4b76
Breyanna Vass <breyannavass@gmail.com>
2020-01-09 12:38:11 -0500

    creating a character animation

--- script.js
+++ script.js
@@ -89,39 +89,51 @@ const ctx = canvas.getContext("2d");
 
 // Animation A
 
-const circle = {
-  x: 200,
+// const circle = {
+//   x: 200,
+//   y: 200,
+//   size: 30,
+//   dx: 5,
+//   dy: 4
+// };
+
+// function drawCircle() {
+//   ctx.beginPath();
+//   ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+//   ctx.fillStyle = "purple";
+//   ctx.fill();
+// }
+
+// function update() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//   drawCircle();
+//   // change position
+//   circle.x += circle.dx;
+//   circle.y += circle.dy;
+//   // Detect side walls
+//   if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+//     circle.dx *= -1;
+//   }
+
+//   // Detect top and bottom walls
+//   if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+//     circle.dy *= -1;
+//   }
+
+//   requestAnimationFrame(update);
+// }
+
+// update();
+
+// Character Animation
+const image = getElementById("source");
+const player = {
+  w: 50,
+  h: 70,
+  x: 20,
   y: 200,
-  size: 30,
-  dx: 5,
-  dy: 4
+  speed: 10,
+  dx: 0,
+  dy: 0
 };
-
-function drawCircle() {
-  ctx.beginPath();
-  ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
-  ctx.fillStyle = "purple";
-  ctx.fill();
-}
-
-function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawCircle();
-  // change position
-  circle.x += circle.dx;
-  circle.y += circle.dy;
-  // Detect side walls
-  if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
-    circle.dx *= -1;
-  }
-
-  // Detect top and bottom walls
-  if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
-    circle.dy *= -1;
-  }
-
-  requestAnimationFrame(update);
-}
-
-update();
